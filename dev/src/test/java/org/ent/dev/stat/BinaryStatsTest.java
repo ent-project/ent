04b527be43c1903b3c728ab86b2194bdb304c971
bastik <phaaurlt@googlemail.com>
2019-06-24 07:15:55 +0200

    make use of BinaryStats info in dev plan

--- dev/src/test/java/org/ent/dev/stat/BinaryStatsTest.java
+++ dev/src/test/java/org/ent/dev/stat/BinaryStatsTest.java
@@ -7,90 +7,105 @@ import org.junit.jupiter.api.Test;
 public class BinaryStatsTest {
 
 	@Test
-	public void onlyHits() {
+	public void noBinsAndHits_onlyHits() {
 		BinaryStats bs = new BinaryStats(4);
 
 		bs.addHit();
-		assertThat(bs.getSize()).isEqualTo(0);
+		assertThat(bs.getNoBins()).isEqualTo(0);
 		bs.addHit();
-		assertThat(bs.getSize()).isEqualTo(0);
+		assertThat(bs.getNoBins()).isEqualTo(0);
 		bs.addHit();
-		assertThat(bs.getSize()).isEqualTo(0);
+		assertThat(bs.getNoBins()).isEqualTo(0);
 		bs.addHit();
-		assertThat(bs.getSize()).isEqualTo(1);
+		assertThat(bs.getNoBins()).isEqualTo(1);
 		assertThat(bs.getHits(0)).isEqualTo(4);
 
 		bs.addHit();
-		assertThat(bs.getSize()).isEqualTo(1);
+		assertThat(bs.getNoBins()).isEqualTo(1);
 		assertThat(bs.getHits(0)).isEqualTo(4);
 		bs.addHit();
-		assertThat(bs.getSize()).isEqualTo(1);
+		assertThat(bs.getNoBins()).isEqualTo(1);
 		assertThat(bs.getHits(0)).isEqualTo(4);
 		bs.addHit();
-		assertThat(bs.getSize()).isEqualTo(1);
+		assertThat(bs.getNoBins()).isEqualTo(1);
 		assertThat(bs.getHits(0)).isEqualTo(4);
 		bs.addHit();
-		assertThat(bs.getSize()).isEqualTo(2);
+		assertThat(bs.getNoBins()).isEqualTo(2);
 		assertThat(bs.getHits(0)).isEqualTo(4);
 		assertThat(bs.getHits(1)).isEqualTo(4);
 	}
 
 	@Test
-	public void onlyMisses() {
+	public void noBinsAndHits_onlyMisses() {
 		BinaryStats bs = new BinaryStats(4);
 
 		bs.addMiss();
-		assertThat(bs.getSize()).isEqualTo(0);
+		assertThat(bs.getNoBins()).isEqualTo(0);
 		bs.addMiss();
-		assertThat(bs.getSize()).isEqualTo(0);
+		assertThat(bs.getNoBins()).isEqualTo(0);
 		bs.addMiss();
-		assertThat(bs.getSize()).isEqualTo(0);
+		assertThat(bs.getNoBins()).isEqualTo(0);
 		bs.addMiss();
-		assertThat(bs.getSize()).isEqualTo(1);
+		assertThat(bs.getNoBins()).isEqualTo(1);
 		assertThat(bs.getHits(0)).isEqualTo(0);
 
 		bs.addMiss();
-		assertThat(bs.getSize()).isEqualTo(1);
+		assertThat(bs.getNoBins()).isEqualTo(1);
 		assertThat(bs.getHits(0)).isEqualTo(0);
 		bs.addMiss();
-		assertThat(bs.getSize()).isEqualTo(1);
+		assertThat(bs.getNoBins()).isEqualTo(1);
 		assertThat(bs.getHits(0)).isEqualTo(0);
 		bs.addMiss();
-		assertThat(bs.getSize()).isEqualTo(1);
+		assertThat(bs.getNoBins()).isEqualTo(1);
 		assertThat(bs.getHits(0)).isEqualTo(0);
 		bs.addMiss();
-		assertThat(bs.getSize()).isEqualTo(2);
+		assertThat(bs.getNoBins()).isEqualTo(2);
 		assertThat(bs.getHits(0)).isEqualTo(0);
 		assertThat(bs.getHits(1)).isEqualTo(0);
 	}
 
 	@Test
-	public void mixed() {
+	public void noBinsAndHits_mixed() {
 		BinaryStats bs = new BinaryStats(4);
 
 		bs.addMiss();
-		assertThat(bs.getSize()).isEqualTo(0);
+		assertThat(bs.getNoBins()).isEqualTo(0);
 		bs.addHit();
-		assertThat(bs.getSize()).isEqualTo(0);
+		assertThat(bs.getNoBins()).isEqualTo(0);
 		bs.addMiss();
-		assertThat(bs.getSize()).isEqualTo(0);
+		assertThat(bs.getNoBins()).isEqualTo(0);
 		bs.addHit();
-		assertThat(bs.getSize()).isEqualTo(1);
+		assertThat(bs.getNoBins()).isEqualTo(1);
 		assertThat(bs.getHits(0)).isEqualTo(2);
 
 		bs.addHit();
-		assertThat(bs.getSize()).isEqualTo(1);
+		assertThat(bs.getNoBins()).isEqualTo(1);
 		assertThat(bs.getHits(0)).isEqualTo(2);
 		bs.addHit();
-		assertThat(bs.getSize()).isEqualTo(1);
+		assertThat(bs.getNoBins()).isEqualTo(1);
 		assertThat(bs.getHits(0)).isEqualTo(2);
 		bs.addHit();
-		assertThat(bs.getSize()).isEqualTo(1);
+		assertThat(bs.getNoBins()).isEqualTo(1);
 		assertThat(bs.getHits(0)).isEqualTo(2);
 		bs.addMiss();
-		assertThat(bs.getSize()).isEqualTo(2);
+		assertThat(bs.getNoBins()).isEqualTo(2);
 		assertThat(bs.getHits(0)).isEqualTo(2);
 		assertThat(bs.getHits(1)).isEqualTo(3);
 	}
 
+	@Test
+	public void noEventsAndTotalHits() {
+		BinaryStats bs = new BinaryStats(4);
+
+		bs.addHit();
+		bs.addMiss();
+		bs.addMiss();
+		bs.addHit();
+
+		bs.addHit();
+		bs.addHit();
+
+		assertThat(bs.getNoEvents()).isEqualTo(6);
+		assertThat(bs.getTotalHits()).isEqualTo(4);
+	}
 }
