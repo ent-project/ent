d95ddc23d2e3e4980d444e7682d717cf23c646ae
bastik <phaaurlt@gmail.com>
2023-07-09 08:08:42 +0200

    new game "two number arithmetic"

---
+++ dev/src/main/java/org/ent/dev/game/forwardarithmetic/TwoNumberArithmeticForwardGame.java
@@ -0,0 +1,171 @@
+package org.ent.dev.game.forwardarithmetic;
+
+import org.apache.commons.rng.UniformRandomProvider;
+import org.ent.Ent;
+import org.ent.LazyPortalArrow;
+import org.ent.PortalArrow;
+import org.ent.RootPortalArrow;
+import org.ent.net.Net;
+import org.ent.net.Purview;
+import org.ent.net.io.formatter.NetFormatter;
+import org.ent.net.node.Node;
+import org.ent.net.node.cmd.Commands;
+import org.ent.net.node.cmd.accessor.Accessors;
+import org.ent.net.node.cmd.operation.Operations;
+import org.ent.net.node.cmd.operation.TriOperation;
+import org.ent.net.node.cmd.veto.Conditions;
+import org.ent.net.node.cmd.veto.Vetos;
+import org.ent.net.util.DotRenderer;
+import org.ent.run.EntRunner;
+import org.ent.webui.WebUI;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.ent.util.NetBuilder.builder;
+import static org.ent.util.NetBuilder.ignored;
+import static org.ent.util.NetBuilder.node;
+import static org.ent.util.NetBuilder.value;
+
+public class TwoNumberArithmeticForwardGame {
+    public static final TriOperation[] TRI_OPERATIONS = new TriOperation[]{
+            Operations.PLUS_OPERATION,
+            Operations.MINUS_OPERATION,
+            Operations.MULTIPLY_OPERATION,
+            Operations.MODULO_OPERATION,
+            Operations.BITWISE_OR_OPERATION,
+            Operations.BITWISE_AND_OPERATION,
+            Operations.XOR_OPERATION,
+            Operations.ROTATE_RIGHT_OPERATION,
+            Operations.SHIFT_LEFT_OPERATION,
+            Operations.SHIFT_RIGHT_OPERATION
+    };
+
+    private static final Logger log = LoggerFactory.getLogger(TwoNumberArithmeticForwardGame.class);
+
+    private final int operand1;
+    private final int operand2;
+    private final TriOperation operation;
+    private final Ent ent;
+    private final int maxSteps;
+
+    private boolean verbose;
+    private NetFormatter formatter;
+
+    private Net verifierNet;
+    private Net answerNet;
+    private Node answerNode;
+    private int verifierPortalCode1;
+    private int verifierPortalCode2;
+
+    public TwoNumberArithmeticForwardGame(int operand1, int operand2, TriOperation operation, Net net, int maxSteps) {
+        this.operand1 = operand1;
+        this.operand2 = operand2;
+        this.operation = operation;
+        this.ent = buildEnt(net);
+        this.maxSteps = maxSteps;
+    }
+
+    public int getVerifierPortalCode1() {
+        return verifierPortalCode1;
+    }
+
+    public int getVerifierPortalCode2() {
+        return verifierPortalCode2;
+    }
+
+    public void setVerbose(boolean verbose) {
+        this.verbose = verbose;
+        this.formatter = new NetFormatter();
+    }
+
+    public static TriOperation drawOperation(UniformRandomProvider random) {
+        return TRI_OPERATIONS[random.nextInt(TRI_OPERATIONS.length)];
+    }
+
+    public static int drawOperand(UniformRandomProvider random) {
+        // modulo: second operand should not be 0 -> some lower bound > 0
+        return random.nextInt(3, 100);
+    }
+
+    private Ent buildEnt(Net net) {
+        Ent ent = new Ent(net);
+        verifierPortalCode1 = ent.addPortal(new LazyPortalArrow(() -> {
+            if (this.verifierNet == null) {
+                this.verifierNet = buildVerifier();
+            }
+            return new RootPortalArrow(this.verifierNet);
+        }));
+        verifierPortalCode2 = ent.addPortal(new LazyPortalArrow(() -> {
+            if (this.verifierNet == null) {
+                this.verifierNet = buildVerifier();
+            }
+            return new RootPortalArrow(this.verifierNet);
+        }));
+        return ent;
+    }
+
+    private Net buildVerifier() {
+        Node portalAnswer, answerCopy, solution;
+        Net verifierNet = builder().net(node(Commands.get(Operations.SET_VALUE_OPERATION, Accessors.RIGHT, Accessors.LEFT),
+                portalAnswer = node(ignored(), answerCopy = node()),
+                node(Commands.get(this.operation, Accessors.RIGHT, Accessors.LEFT_LEFT, Accessors.LEFT_RIGHT),
+                        node(
+                                node(value(this.operand1), value(this.operand2)),
+                                solution = node()
+                        ),
+                        node(Commands.get(Operations.SET_OPERATION, Accessors.FLOW, Accessors.RIGHT),
+                                node(Vetos.get(Conditions.SAME_VALUE_CONDITION, Accessors.LEFT_LEFT, Accessors.LEFT_RIGHT),
+                                        node(answerCopy, solution),
+                                        value(Commands.FINAL_SUCCESS)),
+                                value(Commands.FINAL_FAILURE)))));
+        verifierNet.setPermittedToWrite(false);
+        verifierNet.setPermittedToEvalRoot(true);
+        ent.addDomain(verifierNet);
+
+        answerNet = builder().net(answerNode = value(0));
+        answerNet.setPermittedToWrite(true);
+        ent.addDomain(answerNet);
+        int answerPortalCode = ent.addPortal(new PortalArrow(answerNet));
+        portalAnswer.setValue(answerPortalCode);
+
+        return verifierNet;
+    }
+
+    public void execute() {
+        EntRunner runner = new EntRunner(ent);
+        if (verbose) {
+            log.info("ent {}", formatter.format(ent));
+            String dot = DotRenderer.render(ent.getNet());
+            WebUI.broadcastDot(dot);
+        }
+        for (int step = 0; step < maxSteps; step++) {
+            runner.step();
+            if (verbose) {
+                dumpResults();
+                log.info("after step {}: {}", step, formatter.format(ent));
+                String dot = DotRenderer.render(ent.getNet());
+                WebUI.broadcastDot(dot);
+                String dot2 = DotRenderer.render(verifierNet);
+                WebUI.broadcastDot(dot2);
+                String dot3 = DotRenderer.render(answerNet);
+                WebUI.broadcastDot(dot3);
+            }
+        }
+    }
+
+    private void dumpResults() {
+
+    }
+
+
+    public boolean passedVerifierFinished() {
+        return verifierNet != null &&
+                (verifierNet.getRoot().getValue(Purview.DIRECT) == Commands.FINAL_SUCCESS.getValue() ||
+                        verifierNet.getRoot().getValue(Purview.DIRECT) == Commands.FINAL_FAILURE.getValue());
+    }
+
+    public boolean passedVerifierFinishedSuccessfully() {
+        return verifierNet != null &&
+                verifierNet.getRoot().getValue(Purview.DIRECT) == Commands.FINAL_SUCCESS.getValue();
+    }
+}
---
+++ dev/src/test/java/org/ent/dev/game/forwardarithmetic/TwoNumberArithmeticForwardGameTest.java
@@ -0,0 +1,85 @@
+package org.ent.dev.game.forwardarithmetic;
+
+import org.assertj.core.api.Assertions;
+import org.ent.Profile;
+import org.ent.net.Net;
+import org.ent.net.node.Node;
+import org.ent.net.node.cmd.Commands;
+import org.ent.net.node.cmd.accessor.Accessors;
+import org.ent.net.node.cmd.operation.Operations;
+import org.ent.webui.WebUI;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.ent.util.NetBuilder.builder;
+import static org.ent.util.NetBuilder.ignored;
+import static org.ent.util.NetBuilder.node;
+import static org.ent.util.NetBuilder.unary;
+
+class TwoNumberArithmeticForwardGameTest {
+
+    @BeforeAll
+    static void setTestEnvironment() {
+        Profile.setTest(true);
+    }
+
+    @Test
+    void guiTest() {
+        WebUI.setUpJavalin();
+        WebUI.loopForever();
+    }
+
+    @Test
+    void test() {
+        WebUI.setUpJavalin();
+
+        Node verifierPortal1, verifierPortal2;
+        Node operationCopy, operand1Copy, operand2Copy, operandsCopy, solutionTarget;
+        Node operationCopyContinuation;
+        Net net = builder().net(
+                // copy operation
+                node(Commands.get(Operations.SET_VALUE_OPERATION, Accessors.RIGHT, Accessors.LEFT_RIGHT),
+                        verifierPortal1 = node(
+                                ignored(),
+                                operationCopy = unary(node(operandsCopy = node(operand1Copy = node(), operand2Copy = node()), solutionTarget = node()))),
+                        // point second verifier portal deeper into the verifier
+                        node(Commands.get(Operations.SET_OPERATION, Accessors.LEFT, Accessors.LEFT_RIGHT_LEFT),
+                                verifierPortal2 = node(ignored(), operandsCopy),
+                                // copy first operand
+                                node(Commands.get(Operations.SET_VALUE_OPERATION, Accessors.RIGHT_LEFT, Accessors.LEFT_LEFT_LEFT),
+                                        verifierPortal2,
+                                        // copy second operand
+                                        node(Commands.get(Operations.SET_VALUE_OPERATION, Accessors.RIGHT_RIGHT, Accessors.LEFT_LEFT_RIGHT),
+                                                verifierPortal2,
+                                                // evaluate the copied operation
+                                                operationCopy)))),
+                operationCopyContinuation =
+                        // submit the evaluated answer
+                        node(Commands.get(Operations.SET_VALUE_OPERATION, Accessors.LEFT_LEFT_LEFT, Accessors.RIGHT_LEFT_RIGHT),
+                                verifierPortal1,
+                                node(Commands.get(Operations.EVAL_FLOW_OPERATION, Accessors.LEFT),
+                                        verifierPortal1,
+                                        node(Commands.get(Operations.EVAL_FLOW_OPERATION, Accessors.LEFT),
+                                                verifierPortal1,
+                                                node(Commands.get(Operations.EVAL_FLOW_OPERATION, Accessors.LEFT),
+                                                        verifierPortal1,
+                                                        node(Commands.FINAL_SUCCESS))))
+                        ));
+
+        operationCopy.setRightChild(operationCopyContinuation);
+
+
+        TwoNumberArithmeticForwardGame game = new TwoNumberArithmeticForwardGame(7, 3, Operations.MULTIPLY_OPERATION, net, 15);
+
+        verifierPortal1.setValue(game.getVerifierPortalCode1());
+        verifierPortal2.setValue(game.getVerifierPortalCode2());
+
+        game.setVerbose(true);
+        game.execute();
+
+//        WebUI.loopForever();
+        Assertions.assertThat(game.passedVerifierFinished()).isTrue();
+        Assertions.assertThat(game.passedVerifierFinishedSuccessfully()).isTrue();
+    }
+
+}
