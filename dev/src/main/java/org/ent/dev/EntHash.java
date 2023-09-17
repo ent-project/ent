8d67a55322956c57429398154756dfbb31bc6a1f
bastik <phaaurlt@gmail.com>
2023-09-17 08:54:26 +0200

    fix hasher to take child nodes from other nets into consideration

--- dev/src/main/java/org/ent/dev/EntHash.java
+++ dev/src/main/java/org/ent/dev/EntHash.java
@@ -15,6 +15,10 @@ public class EntHash {
     private static final HashFunction MURMUR_3 = Hashing.murmur3_32_fixed(MURMUR_SEED);
     public static final int DOMAIN_SEPARATOR = 0xfe110e57;
 
+    // xor with net index to reduce chances it gets mixed up with other types of indices
+    // that happen to have the same value
+    public static final int NET_INDEX_MODIFIER = 0b1011011010010100100100010100110;
+
     public static int hash(Ent ent) {
         Hasher hasher = MURMUR_3.newHasher();
         doHash(ent.getNet(), hasher);
@@ -41,8 +45,16 @@ public class EntHash {
             if (node != null) {
                 hasher.putInt(i);
                 hasher.putInt(node.getValue(Permissions.DIRECT));
-                hasher.putInt(node.getLeftChild(Permissions.DIRECT).getIndex());
-                hasher.putInt(node.getRightChild(Permissions.DIRECT).getIndex());
+                Node leftChild = node.getLeftChild(Permissions.DIRECT);
+                hasher.putInt(leftChild.getIndex());
+                if (leftChild.getNet() != net) {
+                    hasher.putInt(leftChild.getNet().getNetIndex() ^ NET_INDEX_MODIFIER);
+                }
+                Node rightChild = node.getRightChild(Permissions.DIRECT);
+                hasher.putInt(rightChild.getIndex());
+                if (rightChild.getNet() != net) {
+                    hasher.putInt(rightChild.getNet().getNetIndex() ^ NET_INDEX_MODIFIER);
+                }
             }
         }
     }
